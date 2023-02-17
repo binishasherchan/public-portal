62c3ea94d688d1b294b924020d3573d8b3ca09c1
binisha <sherchanbinisha5@gmail.com>
2023-02-17 14:01:22 +0545

    Add fix nav on scroll

--- src/pages/Pagenotfound.jsx
+++ src/pages/Pagenotfound.jsx
@@ -6,7 +6,7 @@ export default function Pagenotfound() {
    <>
        <div className='h-screen'>
          <div className='text-center'>
-         <img src="/404.png" alt="" className='w-1/2 mx-auto' />
+         <img src="/404.png" alt="" className=' w-1/3 mx-auto' />
              <a href="/" class="rounded relative d-inline-flex group items-center justify-center px-3.5 py-2 mt-3
                 cursor-pointer border-b-4 border-l-2 active:border-[6eccaf] active:shadow-none shadow-lg bg-gradient-to-tr from-[#6eccaf] to-[#86ddc1] border-[#58e3b7] text-white">            
                  <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
--- src/sections/Home/Section2/Section2.jsx
+++ src/sections/Home/Section2/Section2.jsx
@@ -37,7 +37,7 @@ function Section2() {
       } 
    ]);
   return <section className=" rounded-lg" id="about">
-            <div class="container mx-auto xl:flex pt-5 pb-4">
+            <div class="container mx-auto xl:flex pt-5 pb-4g">
          <div class="lg:flex align-items-center main-col">
              
                         <div class="lg:grid lg:grid-cols-3 gap-4 " >
